5fa8f51a1b7e14eaf7beb1bd64a2ef1e05b22ad3
guyalfasi <guyalfasi@gmail.com>
2024-05-02 11:05:15 +0300

    added the rest of the endpoints

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,23 @@ import bodyParser from 'koa-bodyparser'
 import jwt from 'jsonwebtoken';
 import koaJwt from 'koa-jwt';
 import { query } from './db';
-import cookie from 'koa-cookie';
 import 'dotenv/config'
 
 const app: Koa = new Koa();
 const router: Router = new Router();
 
 app.use(bodyParser());
-app.use(cookie());
 
 app.use(koaJwt({
     secret: process.env.JWT_SECRET as string,
     passthrough: true,
-}).unless({ path: [/^\/login/] }));
+}));
 
 const authenticate = async (ctx: Context, next: Next) => {
     const token = ctx.cookies.get('auth');
     if (!token) {
         ctx.status = 401;
-        ctx.body = { message: 'Authentication Error: No session token provided' };
+        ctx.body = { message: 'Authentication Error: User not logged in' };
         return;
     }
 
@@ -31,14 +29,20 @@ const authenticate = async (ctx: Context, next: Next) => {
         ctx.state.user = decodedSessionUser;
         await next();
     } catch (err) {
-        ctx.status = 401;
+        ctx.status = 400;
         ctx.body = { message: 'Authentication Error: Invalid token' };
     }
 }
 
+let arr: (number | string)[] = [1, "2", 3]
+
 interface LoginRequestEndpoint {
     username: string;
     password: string;
+}
+
+interface ArrayEndpoint {
+    value: number;
 }
 
 router.post('/login', async ctx => {
@@ -52,7 +56,7 @@ router.post('/login', async ctx => {
         const token = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '1h' });
 
         ctx.cookies.set('auth', token, { httpOnly: true });
-        ctx.status = 200;
+        ctx.status = 201;
         ctx.body = { message: 'Login Successful', token };
     } else {
         ctx.status = 400;
@@ -61,14 +65,9 @@ router.post('/login', async ctx => {
 });
 
 router.get('/', authenticate, async ctx => {
-    if (ctx.state.user) {
-        const user = ctx.state.user;
-        const currentDate = new Date().toLocaleDateString();
-        ctx.body = { message: `Hello ${user.username}, today is ${currentDate}` };
-    } else {
-        ctx.status = 400
-        ctx.body = { message: "Unauthorized Access" }
-    }
+    const user = ctx.state.user;
+    const currentDate = new Date().toLocaleDateString();
+    ctx.body = { message: `Hello ${user.username}, today is ${currentDate}` };
 });
 
 router.get('/echo', authenticate, async ctx => {
@@ -78,6 +77,79 @@ router.get('/echo', authenticate, async ctx => {
     } else {
         ctx.status = 401;
         ctx.body = { message: `Message field missing` }
+    }
+})
+
+router.get('/array', authenticate, async ctx => {
+    ctx.body = { array: arr }
+})
+
+router.get('/array/:index', authenticate, async (ctx) => {
+    const index = parseInt(ctx.params.index);
+    if (arr[index]) {
+        ctx.body = { value: arr[index] };
+    } else {
+        ctx.status = 404;
+        ctx.body = { message: "Index out of bounds" };
+    }
+})
+
+router.post('/array', authenticate, async (ctx) => {
+    const { value } = ctx.request.body as ArrayEndpoint;
+    if (ctx.state.user.username.startsWith('admin')) {
+        arr = [...arr, value]
+        ctx.body = { message: "Array updated", array: arr }
+    } else {
+        ctx.status = 403
+        ctx.body = { message: "You don't have permission to do that" }
+    }
+})
+
+router.put("/array/:index", authenticate, async (ctx) => {
+    const { value } = ctx.request.body as ArrayEndpoint;
+    const index = parseInt(ctx.params.index);
+    if (ctx.state.user.username.startsWith("admin")) {
+            if (arr[index]) {
+                arr[index] = value;
+                ctx.body = { message: "Array updated", array: arr };
+            } else {
+                ctx.status = 404;
+                ctx.body = { message: "Index out of bounds" };
+            }
+    } else {
+        ctx.status = 403;
+        ctx.body = { message: "You don't have permission to do that" };
+    }
+});
+
+router.delete('/array', authenticate, async (ctx) => {
+    if (ctx.state.user.username.startsWith("admin")) {
+        if (arr) {
+            arr.pop();
+            ctx.body = { message: "Array updated", array: arr }    
+        } else {
+            ctx.status = 400;
+            ctx.body = { message: "Array is empty" }
+        }
+    } else {
+        ctx.status = 403;
+        ctx.body = { message: "You don't have permission to do that" };
+    }
+})
+
+router.delete('/array/:index', authenticate, async (ctx) => {
+    const index = parseInt(ctx.params.index);
+    if (ctx.state.user.username.startsWith("admin")) {
+        if (arr) {
+            arr[index] = 0;
+            ctx.body = { message: "Array updated", array: arr }    
+        } else {
+            ctx.status = 400;
+            ctx.body = { message: "Array is empty" }
+        }
+    } else {
+        ctx.status = 403;
+        ctx.body = { message: "You don't have permission to do that" };
     }
 })
 
